aa58dec0639f842c1004ed1e686949a60155dff9
Victor Brigido <victorbrigido@icloud.com>
2024-01-05 13:55:58 -0300

    commit app

--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -8,11 +8,109 @@
 import SwiftUI
 
 struct WeatherView: View {
+    var weather: ResponseBody
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                        .padding(.leading)
+                    Text("Hoje \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                        .padding(.leading)
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size:40))
+                            
+                            Text("\(weather.weather[0].main)")
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        .padding(.leading,10)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size:100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+            }
+            .padding(.bottom)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Tempo agora")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", 
+                                   name: "Temp min.",
+                                   value: (weather.main.tempMin.roundDouble() + "°"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer",
+                                   name: "Temp máx",
+                                   value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    HStack {
+                        WeatherRow(logo: "wind",
+                                   name: "Ventos",
+                                   value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity",
+                                   name: "Umidade",
+                                   value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656,
+                                       saturation: 0.787,
+                                       brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656,
+                          saturation: 0.787,
+                          brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
-    WeatherView()
+    WeatherView(weather: previewWeather)
 }
--- WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/Views/WelcomeView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack (spacing: 20) {
+                Text("Bem-vindo ao Weather App")
+                    .bold().font(.title)
+                Text("Compartilhe sua localização atual para saber a previsão do tempo na sua região")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight:.infinity )
     }
 }
 
